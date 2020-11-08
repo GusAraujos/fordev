332e5079febc800b968a8ad8fb5e6fdeb09492c5
Gustavo Araujo <ovatsug12@hotmail.com>
2020-11-08 14:34:19 -0300

    move duplicated code to the setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -14,14 +14,16 @@ void main() {
   RemoteAuthentication sut;
   HttpClientSpy httpClient;
   String url;
+  AuthenticationParams params;
+
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
   });
   test('Should call HttpClient with correct values', () async {
-    final params = AuthenticationParams(
-        email: faker.internet.email(), secret: faker.internet.password());
     await sut.auth(params);
     verify(httpClient.request(
         url: url,
@@ -35,8 +37,7 @@ void main() {
             method: anyNamed('method'),
             body: anyNamed('body')))
         .thenThrow(HttpError.badRequest);
-    final params = AuthenticationParams(
-        email: faker.internet.email(), secret: faker.internet.password());
+
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.unexpected));
